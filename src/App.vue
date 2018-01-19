d4f9a3c0af1f31e258a93a85bebf1b33c95cbca5
alexander.kravchuk <alexander.kravchuk@nakitel.com>
2018-01-19 18:11:28 +0200

    add store

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,10 @@
     <nav class="navbar is-info">
       <div class="container">
         <div class="navbar-brand">
-          <a class="navbar-item" href="https://bulma.io">
+          <router-link class="navbar-item" to="/">
             <img src="https://bulma.io/images/bulma-logo-white.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112"
               height="28">
-          </a>
+          </router-link>
           <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
             <span></span>
             <span></span>
